fae0c3b3aa5f9287c5349e6737ba8c074f0f64d8
Muggie <oceanrabbit0101@protonmail.com>
2024-03-04 08:43:47 +0100

    setting up the project

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>IM-BD Movie page</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
